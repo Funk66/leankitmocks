8239585939acaf41472daf1dab126c9b37f83885
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2017-08-28 16:58:21 +0200

    Made routes case insensitive

--- leankitmocks/__init__.py
+++ leankitmocks/__init__.py
@@ -3,10 +3,12 @@
 
 from json import load
 from os import path
+
+import leankit
 from leankit import *
 
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 
 class Mock():
@@ -15,10 +17,11 @@ class Mock():
     def get(self, url):
         log.debug(f'GET {url}')
         try:
-            with open(path.join(self.folder, url.strip('/') + '.json')) as data:
+            filename = path.join(self.folder, url.lower().strip('/') + '.json')
+            with open(filename) as data:
                 return load(data)
         except FileNotFoundError:
             raise ConnectionError('Server responded with code 404')
 
 
-kanban.api = Mock()
+leankit.api = leankit.kanban.api = Mock()
